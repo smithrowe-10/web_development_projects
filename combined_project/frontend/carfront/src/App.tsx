959acb99b898bdf9a839959b35b9fb7b7de7fd79
smithrowe-10 <smj0418smj@gmail.com>
2025-04-03 21:32:47 +0900

    feat:20250403 carlist get request

--- combined_project/frontend/carfront/src/App.tsx
+++ combined_project/frontend/carfront/src/App.tsx
@@ -3,6 +3,10 @@ import  Toolbar  from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Carlist from './components/Carlist';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
@@ -15,7 +19,9 @@ function App() {
             </Typography>
           </Toolbar>
         </AppBar>
-
+        <QueryClientProvider client={queryClient}>
+          <Carlist/>
+        </QueryClientProvider>
     </Container>
   )
 }
---
+++ combined_project/frontend/carfront/src/components/Carlist.tsx
@@ -0,0 +1,50 @@
+import { CarResponse } from "../types";
+import axios from "axios";
+import { useQuery } from '@tanstack/react-query';
+
+function Carlist() {
+  const getCars = async (): Promise<CarResponse[]> => {
+    const response = await axios.get("http://localhost:8080/api/cars");
+
+    return response.data._embedded.cars;
+  }
+
+  const { data, error, isSuccess } = useQuery({
+    queryKey: ["cars"],
+    queryFn: getCars
+  });
+
+  if(!isSuccess) {
+    return <span>Loading 중...🎈</span>
+  }
+  else if (error) {
+    return <span>자동차 데이터 가져오기 중 오류 발생 😅</span>
+  }
+  else {
+    return(
+      <table>
+        <tbody>
+          {
+            data.map((car: CarResponse) =>
+              <tr key={car._links.self.href}>
+                <td>{car.brand}</td>
+                <td>{car.model}</td>
+                <td>{car.color}</td>
+                <td>{car.registrationNumber}</td>
+                <td>{car.modelYear}</td>
+                <td>{car.price}</td>
+              </tr>)
+          }
+        </tbody>
+      </table>
+    )
+  }
+
+  return(
+    <>
+    
+    </>
+  )
+}
+
+export default Carlist;
